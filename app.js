f9985a12f7311fdebc424701851d6a0b5984eac1
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2021-05-01 03:46:09 +0500

    changes is been made in the componenets

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 $( document ).ready(function() {
     console.log('changes');
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        dots:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:3
+            600: {
+                items: 2
             },
-            1000:{
-                items:5
+            1000: {
+                items: 4
             }
         }
-    })
+    });
 });
 
 
