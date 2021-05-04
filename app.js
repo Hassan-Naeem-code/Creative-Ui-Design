47c94fc5f654c97c652676119c24ec967d5381c9
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2021-05-05 00:32:41 +0500

    added some changes in the UI

--- app.js
+++ app.js
@@ -18,6 +18,24 @@ $( document ).ready(function() {
             }
         }
     });
+    $('.owl-carousel-second').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        dots: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 2
+            }
+        }
+    });
 });
 
 
